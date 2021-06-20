c7c8ef10dc9bbba6893c4df4c53a034bd11d4483
Pedro Elias <pedro.abreu@enacom.com>
2021-06-20 18:05:05 -0300

    Inicio do desenvolvimento

--- Primeira Entrega/Lexico/Compilador/src/main/java/lexico/Lexer.java
+++ Primeira Entrega/Lexico/Compilador/src/main/java/lexico/Lexer.java
@@ -3,12 +3,54 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lexico;
 
+        
+package lexico;
+import java.io.*;
+import java.util.*;
 /**
  *
  * @author pedroelias
  */
 public class Lexer {
+
+    public static int line = 1;
+    private char ch = ' ';
+    private FileReader file;
     
+    private Hashtable words = new Hashtable();
+    
+    private void reserve(Word w){
+        words.put(w.getLexeme(), w);
+    }
+    
+    public Lexer(String fileName) throws FileNotFoundException{
+    
+        try{
+            file = new FileReader(fileName);
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Arquivo não encontrado");
+            throw e;
+        }
+        
+        reserve(new Word("if",Tag.IF));
+        reserve(new Word("class",Tag.CLASS));
+        reserve(new Word("int",Tag.INT));
+        reserve(new Word("string",Tag.STRING));
+        reserve(new Word("float",Tag.FLOAT));
+        reserve(new Word("init",Tag.INIT));
+        reserve(new Word("stop",Tag.STOP));
+        reserve(new Word("else",Tag.ELSE));
+        reserve(new Word("do",Tag.DO));
+        reserve(new Word("while",Tag.WHILE));
+        reserve(new Word("read",Tag.READ));
+        reserve(new Word("write",Tag.WRITE));
+        reserve(new Word("if",Tag.IF));
+        
+        
+    }
+    
+    
+
 }
--- Primeira Entrega/Lexico/Compilador/src/main/java/lexico/Token.java
+++ Primeira Entrega/Lexico/Compilador/src/main/java/lexico/Token.java
@@ -10,5 +10,13 @@ package lexico;
  * @author pedroelias
  */
 public class Token {
+    public final int tag;
     
+    public Token(int t){
+        tag = t;
+    }
+    
+    public String toString(){
+        return "" + tag;
+    }
 }
